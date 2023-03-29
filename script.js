4d07f3a4c35a09a6c2e679e37d98efc16082b0d6
Runner-Dad84 <vanhoogie@gmail.com>
2023-03-28 20:38:08 -0400

    Square function to build etch-a-sketch.

---
+++ script.js
@@ -0,0 +1,11 @@
+let container = document.querySelector('#container');
+
+for (i = 0; i < 16; i++){
+    let squares = document.createElement('div');
+    squares.classList.add('squareStyle');
+    container.appendChild(squares);
+};
+
+
+
+
