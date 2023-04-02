83b25acb940a0301a48cb2ab14792a2236e37eb0
Runner-Dad84 <vanhoogie@gmail.com>
2023-04-01 21:54:07 -0400

    Add event listener for mouse enter. Change background color.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let container = document.querySelector('#container');
 
-for (i = 0; i < 8; i++){
+for (i = 0; i < 32; i++){
     let row = document.createElement('div');
     row.classList.add('rowStyle');
     container.appendChild(row);
     
-    for (j = 0; j < 8; j++){
+    for (j = 0; j < 32; j++){
         let column = document.createElement('div');
         column.classList.add('columnStyle');
-        row.appendChild(column);};
+        row.appendChild(column);
+    };
 };
 
+const divs = document.getElementsByClassName('columnStyle');
 
+Array.from(divs).forEach(function(e) {
+    e.addEventListener("mouseenter", function() {
+        this.style.background = "green";
+    });
+});
 
 
