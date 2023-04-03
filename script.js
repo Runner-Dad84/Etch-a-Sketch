70d8d2a8cd4df5c0674129c935af14513d8fac2c
Runner-Dad84 <vanhoogie@gmail.com>
2023-04-02 21:16:05 -0400

    Add button with prompt.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const button = document.getElementById("button");
+button.addEventListener("click", (event) => {
+    prompt("How many squares do you want? Max of 100");
+});
+
+
 let container = document.querySelector('#container');
 
-for (i = 0; i < 32; i++){
+for (i = 0; i < 100; i++){
     let row = document.createElement('div');
     row.classList.add('rowStyle');
     container.appendChild(row);
     
-    for (j = 0; j < 32; j++){
+    for (j = 0; j < 100; j++){
         let column = document.createElement('div');
         column.classList.add('columnStyle');
         row.appendChild(column);
