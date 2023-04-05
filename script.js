3f085c60aba849dc8b8d54ac8ddf9b2be6032174
Runner-Dad84 <vanhoogie@gmail.com>
2023-04-04 21:41:00 -0400

    Move color function inside quares function. Add html span. Add span content num of squares.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const button = document.getElementById("button");
+let span = document.getElementById("squareNum");
+span.textContent = "This Etch A Sketch is size 16x16.";
 let num = 16;
 
-
+//button input for number of squares//
 function functionAsk (){
     let answer = prompt("Enter the number of squares per side on the etch-a-sketch (range 16-100)");
     num = parseInt(answer, 10);
     
     if (Number.isNaN(num)){
         alert("This is not a number")
-    } else if (typeof num === "number" && (num < 100) && (num > 15)) {
-        console.log(num)
+    } else if (typeof num === "number" && (num < 101) && (num > 15)) {
+        num;
+        span.textContent = `This Etch A Sketch is size is ${num}x${num}.`;
+        return squares();
     } else {
             alert("Please try again.")
     };
@@ -17,6 +21,7 @@ function functionAsk (){
 
 let container = document.querySelector('#container');
 
+//creates number of squares and highlighting tool//
 function squares() {
     for (i = 0; i < num; i++){
         let row = document.createElement('div');
@@ -29,16 +34,18 @@ function squares() {
             row.appendChild(column);
         };
     };
+
+    const divs = document.getElementsByClassName('columnStyle');
+
+    Array.from(divs).forEach(function(e) {
+    e.addEventListener("mouseenter", function() {
+    this.style.background = "green";});
+  });
 }
 
+//Call squares function on page load//
 document.addEventListener("DOMContentLoaded", squares());
 
-const divs = document.getElementsByClassName('columnStyle');
 
-Array.from(divs).forEach(function(e) {
-    e.addEventListener("mouseenter", function() {
-        this.style.background = "green";
-    });
-});
 
 
